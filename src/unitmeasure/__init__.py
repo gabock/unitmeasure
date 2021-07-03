35fcf21f2075591c0799fc252e07fecffc2a86eb
Greg Bock <gabock4@gmail.com>
2021-07-02 21:44:32 -0500

    ✨ Add base unit converter

--- src/unitmeasure/__init__.py
+++ src/unitmeasure/__init__.py
@@ -1,2 +1,2 @@
 
-from unitmeasure.unit import Unit
+from unitmeasure.unit import *
--- src/unitmeasure/unit.py
+++ src/unitmeasure/unit.py
@@ -1,6 +1,30 @@
 
 
+class UnitConverter(object):
+    """Describe how to convert a unit to and from the base unit of its dimension.
+
+    The methods of this class define the interface for performing conversions
+    to and from the base unit of a unit's dimension.
+
+    The default implementation simply returns the value.
+    Methods are implemented differently depending on the type of conversion.
+
+    When creating a custom unit converter, BOTH methods must be implemented to 
+    define the conversion to and from a value in terms of a unit.
+    """
+    def baseUnitValue(self, fromValue):
+        """This method takes a value in terms of a unit and returns the corresponding value in terms of the base unit.
+        """
+        return fromValue
+
+    def value(self, fromBasUnitValue):
+        """This method takes in a value in terms of the base unit and returns the equialent value in terms of the unit.
+        """
+        return fromBasUnitValue
+
+
 class Unit(object):
 
     def __init__(self, symbol):
         self.symbol = symbol
+
