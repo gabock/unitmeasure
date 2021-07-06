8754a68ea9abc00d0d3b9ff42d2ba70a6245d340
Greg Bock <gabock4@gmail.com>
2021-07-06 14:06:17 -0500

    ✨ Add unit __eq__ method

--- src/unitmeasure/unit.py
+++ src/unitmeasure/unit.py
@@ -2,3 +2,9 @@ class Unit(object):
 
     def __init__(self, symbol):
         self.symbol = symbol
+
+    def __eq__(self, other):
+        if not isinstance(other, Unit):
+            return False
+
+        return self.symbol == other.symbol
--- tests/unittests/unitmeasure/test_unit.py
+++ tests/unittests/unitmeasure/test_unit.py
@@ -5,3 +5,11 @@ import unitmeasure
 def test_unit_symbol():
     unit = unitmeasure.Unit("s")
     assert unit.symbol == "s"
+
+@pytest.mark.parametrize("unit1, unit2, expected", [
+    (unitmeasure.Unit("s"), unitmeasure.Unit("s"), True),
+    (unitmeasure.Unit("s"), unitmeasure.Unit("a"), False),
+    (unitmeasure.Unit("s"), "s", False),
+])
+def test_is_equal(unit1, unit2, expected):
+    assert (unit1 == unit2) == expected
