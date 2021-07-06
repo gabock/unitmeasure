6ec8895d2d9d7fedd2b08e38278cbdf15497c890
Greg Bock <gabock4@gmail.com>
2021-07-06 13:32:25 -0500

    ✨ Add reciprocal converter

--- src/unitmeasure/converters/__init__.py
+++ src/unitmeasure/converters/__init__.py
@@ -1,2 +1,3 @@
 from unitmeasure.converters.linear import UnitConverterLinear
 from unitmeasure.converters.model import UnitConverter
+from unitmeasure.converters.reciprocal import UnitConverterReciprocal
