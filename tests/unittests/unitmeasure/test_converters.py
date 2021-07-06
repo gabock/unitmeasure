55010f10a9ddbf6910b9f29a0ef8a25195471d2f
Greg Bock <gabock4@gmail.com>
2021-07-05 20:48:28 -0500

    ✅ Update unit test
    update unit test to not include
    param name.

--- tests/unittests/unitmeasure/test_converters.py
+++ tests/unittests/unitmeasure/test_converters.py
@@ -6,5 +6,5 @@ def test_unit_converter_base():
     unit_converter = unitmeasure.converters.UnitConverter()
     # While this test is very simple, it is here to ensure that these
     # methods are maintained as the interface for a unit converter
-    assert unit_converter.baseUnitValue(fromValue=5) == 5
-    assert unit_converter.value(fromBasUnitValue=10) == 10
+    assert unit_converter.baseUnitValue(5) == 5
+    assert unit_converter.value(10) == 10
