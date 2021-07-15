56dccdf12f309d21b770d7c101cbb46be98aa518
Greg Bock <gabock4@gmail.com>
2021-07-15 18:00:32 -0500

    ✨ Add UnitLength

--- src/unitmeasure/units/__init__.py
+++ src/unitmeasure/units/__init__.py
@@ -10,3 +10,4 @@ from unitmeasure.units.electric_resistance import UnitElectricResistance
 from unitmeasure.units.energy import UnitEnergy
 from unitmeasure.units.frequency import UnitFrequency
 from unitmeasure.units.fuel_efficiency import UnitFuelEfficiency
+from unitmeasure.units.length import UnitLength
