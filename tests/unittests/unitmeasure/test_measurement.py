67b82b9847258208f01e0670ef62dbf8707af761
Greg Bock <gabock4@gmail.com>
2021-07-29 01:02:46 -0500

    ✅ Add description tests

--- tests/unittests/unitmeasure/test_measurement.py
+++ tests/unittests/unitmeasure/test_measurement.py
@@ -8,3 +8,11 @@ def test_immutable():
         measure.value = 40
     with pytest.raises(TypeError):
         measure.unit = unitmeasure.UnitDuration.minutes
+
+def test_description():
+    measure = unitmeasure.Measurement(value=60, unit=unitmeasure.UnitDuration.seconds)
+    assert measure.description == "60 s"
+
+def test_debug_description():
+    measure = unitmeasure.Measurement(value=60, unit=unitmeasure.UnitDuration.seconds)
+    assert measure.debugDescription == "60 s"
