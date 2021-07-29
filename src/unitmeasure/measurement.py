7b7adbfb4ba0b014608027bffd362b263bc92c1d
Greg Bock <gabock4@gmail.com>
2021-07-29 00:56:43 -0500

    ♻️ Change to use custom setattr for frozen
    change to use custom setattr and delattr for immutable like class
    over dataclass. doing this so a convert method
    can be supported

--- src/unitmeasure/measurement.py
+++ src/unitmeasure/measurement.py
@@ -1,9 +1,17 @@
 
-from dataclasses import dataclass
-
 from unitmeasure.unit import Unit
 
-@dataclass(frozen=True)
+
 class Measurement(object):
-    value: float
-    unit: Unit
+    
+    __slots__ = ["unit", "value"]
+
+    def __init__(self, value, unit):
+        super().__setattr__("value", value)
+        super().__setattr__("unit", unit)
+
+    def __setattr__(self, *args):
+        raise TypeError("'Measurement' object doesn't support item assignment")
+
+    def __delattr__(self, *args):
+        raise TypeError("'Measurement' object doesn't support item deletion")
